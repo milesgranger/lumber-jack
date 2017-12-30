cf54fd24b7d9871f94c3e080ea52c14a7e121cff
Miles Granger <miles59923@gmail.com>
2017-12-30 17:19:08 +0100

    restructuring to lib

---
+++ lumber-jack/src/series/mod.rs
@@ -0,0 +1,31 @@
+use std::vec::Vec;
+use std::option::Option;
+
+
+pub struct Series {
+    index: Vec<usize>,
+    values: Vec<usize>
+}
+
+impl Series {
+
+    pub fn new(index: Vec<usize>, values: Vec<usize>) -> Option<Series>  {
+        /*
+            Return new Series object, but only if index and values are the same length
+        */
+        if index.len() != values.len() {
+            None
+        }
+        else {
+            Some(Series {index, values})
+        }
+    }
+
+    pub fn length(&self) -> usize {
+        /*
+            Return the length of the Series
+        */
+        self.values.len()
+    }
+
+}
