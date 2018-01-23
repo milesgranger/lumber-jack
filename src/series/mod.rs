c2d3d12fc1df02b84f9656236b9f09f742385e61
Miles Granger <miles59923@gmail.com>
2018-01-23 16:43:39 +0100

    work on core project, datacells and series

--- src/series/mod.rs
+++ src/series/mod.rs
@@ -1,21 +1,66 @@
+extern crate num;
+
+use self::num::{Num, NumCast, Float};
 use std::vec::Vec;
 use std::option::Option;
+use std::result::Result;
+use std::iter;
 
-
-pub struct Series {
-    index: Vec<usize>,
-    values: Vec<usize>
+#[derive(Debug, PartialEq, Clone)]
+pub enum DataType {
+    Numeric,
+    Object
 }
 
-impl Series {
 
-    pub fn new(index: Vec<usize>, values: Vec<usize>) -> Option<Series>  {
+#[derive(Debug, PartialEq, Clone)]
+pub struct DataCell<T> {
+    value: T,
+    data_type: DataType
+}
+
+impl<T> DataCell<T> {
+    pub fn new(value: T, data_type: DataType) -> DataCell<T> {
+        DataCell{
+            value,
+            data_type
+        }
+    }
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Series<T>
+{
+    index: Vec<DataCell<T>>,
+    values: Vec<DataCell<T>>,
+}
+
+
+impl<T> Series<T> {
+
+    pub fn new(index: Vec<T>, values: Vec<T>) -> Series<T> {
         /*
         Return new Series object, but only if index and values are the same length
         return None if failure due to index and values not being same length.
         */
-        if index.len() != values.len() { None }
-        else { Some(Series {index, values}) }
+
+        let mut _index: Vec<DataCell<T>> = Vec::with_capacity(index.len());
+        let mut _values: Vec<DataCell<T>> = Vec::with_capacity(values.len());
+        for val in index {
+            _index.push(DataCell::new(val, DataType::Object));
+        }
+        for val in values {
+            _values.push(DataCell::new(val, DataType::Object));
+        }
+
+        Series {
+            index: _index,
+            values: _values
+        }
+
+
+
     }
 
     pub fn length(&self) -> usize {
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -5,29 +5,8 @@ use self::super::alterations;
 use std::option::Option;
 
 #[test]
-fn series_contruction() {
-    // Test construction of a series via new()
-    let _series: Option<Series> = Series::new(vec![1, 2, 3], vec![1, 2, 3]);
-    match _series {
-        Some(s) => { /* Successfully made series, test passes. */ },
-        _ => { panic!("Unable to create new series!"); }
-    }
+fn series_construction() {
+    let _series = Series::new(vec![0, 1, 2], vec![1, 2, 3]);
+    println!("Got series: {:?}", _series);
 }
 
-#[test]
-fn series_length() {
-    // Test the length method of series
-    let _series: Option<Series> = Series::new(vec![1, 2, 3], vec![1, 2, 3]);
-    if let Some(_series) = _series {
-        assert_eq!(3, _series.length());
-    } else {
-        panic!("Unable to create new series!");
-    }
-}
-
-#[test]
-fn one_hot_encode_text(){
-    // Test alterations::split_n_one_hot_encode
-    let raw_texts = vec!["Hello, there", "Hello, here"];
-    let (words, one_hot) = alterations::split_n_hot_encode(raw_texts, ",", 0);
-}
