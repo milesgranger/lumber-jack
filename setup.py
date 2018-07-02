cf151f66af5f99fa5a1a179622243babe60f96d8
Miles Granger <miles59923@gmail.com>
2018-07-02 17:39:53 +0200

    Update setup.py lib dirs

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 from subprocess import Popen
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
@@ -7,15 +6,17 @@ import numpy as np
 from lumberjack._version import __version__
 
 setup_requires = ['pytest-runner', "Cython"]
-install_requires = ['numpy', 'pandas']
+install_requires = ['numpy']
 tests_require = install_requires + ['pytest==3.5.0', 'pytest-benchmark']
 
 
-class BuildRustSharedObject(build_ext):
-
+class BuildRustLib(build_ext):
+    """
+    Augment the setup.py build_ext command to creat the Rust shared object
+    """
     def run(self):
         print('running build of liblumberjack')
-        process = Popen(['cargo', 'build', '--out-dir', './lumberjack/rust/', '-Z', 'unstable-options'])
+        process = Popen(['cargo', 'build', '--out-dir', './lumberjack/rust/', '-Z', 'unstable-options', '--release'])
         process.wait()
         if process.returncode != 0:
             raise RuntimeError('Failed building liblumberjack object, exit code: {}'.format(process.returncode))
@@ -26,7 +27,7 @@ rust_core_ext = Extension(name="*",
                           sources=["lumberjack/cython/*.pyx"],
                           libraries=['lumberjack'],
                           include_dirs=['./lumberjack/rust/', np.get_include()],
-                          library_dirs=['./lumberjack/rust'])
+                          library_dirs=['./lumberjack/rust/'])
 
 setup(
     name='lumber-jack',
@@ -37,7 +38,7 @@ setup(
     description="Alpha work on adding additional and improved functionality to Pandas.",
     long_description="Alpha work on adding additional and improved functionality to Pandas.",
     cmdclass={
-      'build_ext': BuildRustSharedObject
+      'build_ext': BuildRustLib
     },
     classifiers=[
         'License :: OSI Approved :: MIT License',
@@ -47,6 +48,7 @@ setup(
         'Intended Audience :: Information Technology',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Rust',
@@ -56,15 +58,13 @@ setup(
         'Operating System :: MacOS :: MacOS X',
     ],
     packages=find_packages(),
-    package_data={'lumberjack': ['lumberjack/rust/liblumberjack']},
-    include_dirs=[np.get_include()],
+    include_dirs=[np.get_include(), 'lumberjack/rust/'],
     ext_modules=cythonize([rust_core_ext]),
     install_requires=install_requires,
     tests_require=tests_require,
     test_suite='tests',
     setup_requires=setup_requires + install_requires,
     include_package_data=True,
-
     license="OSI Approved :: BSD License",
     zip_safe=False
 )
