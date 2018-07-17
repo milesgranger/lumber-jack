84d6626cf9e6215a9812f0ae97df7cf29f6570b5
Miles Granger <miles59923@gmail.com>
2018-07-17 16:54:48 +0200

    Update travis

--- build.rs
+++ build.rs
@@ -1,11 +1,7 @@
 // build.rs
 extern crate cbindgen;
 
-use std::env;
-
 fn main() {
-    //let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
     cbindgen::generate(&"./".to_string())
       .unwrap()
       .write_to_file("./lumberjack/rust/liblumberjack.h");
