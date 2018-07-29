d97f945951a1e9d7908aa0f37099a184dd0ff725
Miles Granger <miles59923@gmail.com>
2018-07-29 20:01:13 +0200

    Working, passing of two maps without pickling

--- tests/test_rust_series.py
+++ tests/test_rust_series.py
@@ -27,9 +27,9 @@ class RustSeriesTestCase(unittest.TestCase):
     def test_series_map(self):
         lj_series = lj.Series.arange(0, 10000)
         variable = 2.0
-        result = lj_series.map(lambda: variable, out_dtype=float)
-        result1 = lj_series.map(lambda: variable, out_dtype=float)
-        result2 = lj_series.map(lambda: variable, out_dtype=float)
+        result = lj_series.map(lambda v: variable, out_dtype=float)
+        result1 = lj_series.map(lambda v: 2.0, out_dtype=float)
+        #result2 = lj_series.map(lambda: variable, out_dtype=float)
         #logger.debug('Result from .map() -> {}'.format(result))
 
 
